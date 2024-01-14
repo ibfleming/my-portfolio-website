77d6a89e14bd6b2f205416df0c864edad066f405
ibfleming <ianfleming678@gmail.com>
2024-01-13 21:42:21 -0800

    More revamps and styling changes!

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,39 @@
 function Footer() {
    return (
-      <div className='container mx-auto'>
-         <div className='p-5 bg-gray-200 rounded-lg py-12 m-5'>
-            <div className='flex flex-col '>
-               <h3 className='text-3xl leading-10 text-slate-700 w-full text-center font-bold'>
-                  Contact Me
+      <div className='container mx-auto mb-8'>
+         <div className=' bg-gray-200 rounded-lg p-8'>
+            <div className='flex flex-col'>
+               <h3 className='text-3xl leading-10 text-slate-800 w-full text-center font-bold pb-4'>
+                  Contact Me!
                </h3>
-               <div className='text-base text-slate-800 w-full  md:w-2/4 self-center text-center lg:pt-4'>
+               <div className='text-base text-slate-700 w-1/2 self-center text-center pb-4'>
                   Thanks for scrolling this far! Feel free to send me a message
                   if you have any questions or concerns. Business inquiries
                   would be wonderful.
                </div>
-               <div className='text-base text-slate-800 w-full md:w-2/4 self-center text-center lg:pt-4'>
+               <div className='text-base text-slate-800 w-full md:w-2/4 self-center text-center'>
                   <form
                      className='flex flex-col items-start'
                      action='mailto:your-email@example.com'
                      method='post'
                      encType='text/plain'
                   >
-                     <label className='text-slate-700 mt-1' htmlFor='name'>
+                     <label className='text-slate-800' htmlFor='name'>
                         Name:
                      </label>
                      <input
-                        className='border-2 border-gray-300 rounded px-3 py-2 mt-1 mb-5 text-sm w-full'
+                        className='border-2 border-gray-300 rounded px-3 py-2 text-sm w-full mb-4'
                         type='text'
                         id='name'
                         name='name'
                         required
                      />
 
-                     <label className='text-slate-700 mt-1' htmlFor='email'>
+                     <label className='text-slate-800 mt-1' htmlFor='email'>
                         Email:
                      </label>
                      <input
-                        className='border-2 border-gray-300 rounded px-3 py-2 mt-1 mb-5 text-sm w-full'
+                        className='border-2 border-gray-300 rounded px-3 py-2 text-sm w-full mb-4'
                         type='email'
                         id='email'
                         name='email'
@@ -41,26 +41,26 @@ function Footer() {
                         required
                      />
 
-                     <label className='text-slate-700 mt-1' htmlFor='message'>
+                     <label className='text-slate-800' htmlFor='message'>
                         Message:
                      </label>
                      <textarea
-                        className='border-2 border-gray-300 rounded px-3 py-2 mt-1 mb-5 text-sm w-full'
+                        className='border-2 border-gray-300 rounded px-3 py-2 text-sm w-full'
                         id='message'
                         name='message'
                         required
                      ></textarea>
 
                      <input
-                        className='bg-gradient-to-tr from-slate-500 to-blue-500 hover:from-blue-500 hover:to-slate-500 text-white font-bold py-2 px-4 rounded self-center cursor-pointer'
+                        className='bg-gradient-to-tr from-slate-500 to-blue-500 hover:from-blue-500 hover:to-slate-500 text-white font-bold py-2 px-4 rounded self-center cursor-pointer my-8'
                         type='submit'
                         value='Send'
                      />
                   </form>
                </div>
             </div>
-            <div className='font-bold text-slate-700 content-center'>
-               <ul className='flex flex-row space-x-6 justify-center pt-10'>
+            <div className='font-bold text-slate-700 content-center mb-4'>
+               <ul className='flex flex-row space-x-6 justify-center'>
                   <li>
                      <a href='#' className='hover:underline cursor-pointer'>
                         Home
@@ -84,25 +84,9 @@ function Footer() {
                </ul>
             </div>
 
-            <div className='flex flex-row justify-center text-xs text-slate-400 pt-10'>
-               Copyright &copy; 2024 Ian B. Fleming
+            <div className='flex flex-row justify-center text-xs text-slate-400'>
+               Copyright &copy; 2024 Ian B. Fleming | Created with React
             </div>
-            {/* <div className=''>
-               <ul className='flex flex-row space-x-4 justify-center'>
-                  <li>
-                     <a href='#' target='_blank' rel='nofollow'></a>
-                  </li>
-                  <li>
-                     <a href='#' target='_blank' rel='nofollow'></a>
-                  </li>
-                  <li>
-                     <a href='#' target='_blank' rel='nofollow'></a>
-                  </li>
-                  <li>
-                     <a href='#' target='_blank' rel='nofollow'></a>
-                  </li>
-               </ul>
-            </div> */}
          </div>
       </div>
    );
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,31 +3,35 @@ import ProfilePhotoSmall from '../img/portfolio-profile-picture-small.jpg';
 /* eslint-disable react/no-unescaped-entities */
 function Intro() {
    return (
-      <div className='flex flex-col items-center justify-center mb-16'>
-         <div className='flex items-center justify-center'>
+      <div className='container mx-auto my-8 mt-16'>
+         <div className='flex flex-row space-x-12 items-center justify-center'>
             <img
                src={ProfilePhotoSmall}
                alt='Portfolio Photo'
-               className='rounded-full h-48 w-48 mt-10 mr-8 border-4 border-slate-600'
+               className='rounded-full h-48 w-48 border-4 border-slate-800'
             />
-            <h1 className='font-Ysabeau italic font-semibold text-7xl text-slate-600 pt-16 pb-6'>
+            <h1 className='font-Ysabeau italic font-semibold text-7xl text-slate-800'>
                Ian Byron Fleming
             </h1>
          </div>
-         <div className='container mx-auto text-slate-700 text-center'>
-            <h2 className='text-3xl font-Ysabeau font-normal tracking-wide text-center text-slate-800 my-10'>
-               Software Engineer/Computer Science
+         <div className='flex flex-row items-center justify-center my-8'>
+            <h2 className='bg-gray-200 rounded-md p-4 text-2xl font-Ysabeau border-2 border-slate-800 text-slate-800 '>
+               Software Engineer / Computer Science
             </h2>
-            <div className='p-8 bg-gray-200 rounded-lg m-5'>
-               <h2 className='text-3xl font-bold tracking-wider'>Welcome</h2>
-               <div className='mx-36 pt-4'>
-                  Senior at the University of Idaho pursuing a bachelor's degree
-                  in Computer Science. With a diverse portfolio encompassing
-                  software development, general applications, and web
-                  development, I am eager to leverage my skills in a
+         </div>
+         <div className='bg-gray-200 rounded-lg text-center p-8 border-2 border-slate-800'>
+            <div className='flex flex-col items-center justify-center'>
+               <h1 className='text-3xl text-center font-bold italic pb-4 text-slate-800'>
+                  Welcome to my portfolio!
+               </h1>
+               <div className='text-md w-1/2 self-center text-left text-slate-700 text-lg'>
+                  I am a senior at the University of Idaho pursuing a bachelor's
+                  degree in Computer Science. With a diverse portfolio
+                  encompassing software development, general applications, and
+                  web development, I am eager to leverage my skills in a
                   professional setting. I am actively seeking job opportunities
                   or internships to further immerse myself in the industry and
-                  enhance my value as a developer.
+                  enhance my value as a developer and creator.
                </div>
             </div>
          </div>
